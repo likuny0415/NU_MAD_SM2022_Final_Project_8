ee8b0d3b2d54f2b544c5aee692dbcb0f355fb45e
likuny0415 <li.kuny@husky.neu.edu>
2022-06-05 20:44:15 -0400

    test kuny merge

--- app/src/main/java/com/example/cs4520teamproject/Account.java
+++ app/src/main/java/com/example/cs4520teamproject/Account.java
@@ -3,13 +3,17 @@ package com.example.cs4520teamproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class Account extends AppCompatActivity {
+    private final String TAG = "Demo";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
-        android.util.Log.d(TAG, "onCreate: " + 123);
+        android.util.Log.d("TAG", "onCreate: " + 123);
+        Log.d(TAG, "onCreate: " + " my test");
+
     }
 }
