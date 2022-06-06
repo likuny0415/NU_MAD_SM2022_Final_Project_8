878e58761cc1d5cb825c4a2f24d0c1fbd8633b92
likuny0415 <li.kuny@husky.neu.edu>
2022-06-06 11:15:32 -0400

    test branch

--- app/src/main/java/com/example/cs4520teamproject/MainActivity.java
+++ app/src/main/java/com/example/cs4520teamproject/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.cs4520teamproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    private final String TAG ="Demo";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d(TAG, "onCreate: " + " test branch");
     }
 }
