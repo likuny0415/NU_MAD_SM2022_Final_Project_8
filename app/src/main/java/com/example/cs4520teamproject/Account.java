e2573adc035a6f5e99aa6f1fd358b3ca2aa499df
steveng <stevenguan1123@gmail.com>
2022-06-05 20:44:52 -0400

    test steven merge2

--- app/src/main/java/com/example/cs4520teamproject/Account.java
+++ app/src/main/java/com/example/cs4520teamproject/Account.java
@@ -3,6 +3,7 @@ package com.example.cs4520teamproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class Account extends AppCompatActivity {
 
@@ -10,6 +11,8 @@ public class Account extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
-        android.util.Log.d(TAG, "onCreate: " + 123);
+        android.util.Log.d("TAG", "onCreate: " + 123);
+        Log.d("TAG", "onCreate: " + "test steven merge");
+
     }
 }
